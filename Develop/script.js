04515484c4921f7af6b28ac90fe3388a44022c49
alexben11 <113950983+alexben11@users.noreply.github.com>
2022-10-27 13:37:22 -0400

    Added current date in p id currentday

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,9 @@
 //Display current date in the p currenday id
+var today=moment();
+console.log(today.format("dddd, MMMM Do YYYY, h:mm:ss a"));
+$("#currentDay").text(today);
+
+
 //Add timeblocks for standard bussiness hours
 //Color code each timeblock to indicte past,present,future
 //Able to click into timeblock
@@ -6,4 +11,3 @@
 //  -click save button and save event in timeblock
 //      `text is saved into local storage
 //      'refresh will keep events in local storage
-
