1227f86fbd7840dffe3b060bce59a133c7a5c5c3
alexben11 <113950983+alexben11@users.noreply.github.com>
2022-10-26 22:27:12 -0400

    entered criteria comments in script

---
+++ Develop/script.js
@@ -0,0 +1,9 @@
+//Display current date in the p currenday id
+//Add timeblocks for standard bussiness hours
+//Color code each timeblock to indicte past,present,future
+//Able to click into timeblock
+//  -can enter an event into timeblock
+//  -click save button and save event in timeblock
+//      `text is saved into local storage
+//      'refresh will keep events in local storage
+
