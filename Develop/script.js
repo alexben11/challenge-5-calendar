4887c8b5010abe4eed8bf3366dc19711da5f6ce5
alexben11 <113950983+alexben11@users.noreply.github.com>
2022-10-27 15:45:47 -0400

    Added timeblocks

--- Develop/script.js
+++ Develop/script.js
@@ -4,8 +4,31 @@ console.log(today.format("dddd, MMMM Do YYYY, h:mm:ss a"));
 $("#currentDay").text(today);
 
 
-//Add timeblocks for standard bussiness hours
-//Color code each timeblock to indicte past,present,future
+//Add timeblocks for standard bussiness hours in HTML.
+
+//Color code timeblock in reference to past,present,future times.
+// Create an if statement that compares div id with hour in moment or moment format and chance styles accordingly.
+var currentTime = today.format("H");
+console.log (currentTime);
+
+//create variables for each timeblock
+
+var nine= document.getElementById("#9");
+var ten= document.getElementById("#10");
+var eleven= document.getElementById("#11");
+var twelve= document.getElementById("#12");
+var thirteen= document.getElementById("#13");
+var fourteen= document.getElementById("#14");
+var fifteen= document.getElementById("#15");
+var sixteen= document.getElementById("#16");
+var sixteen= document.getElementById("#17");
+ console.log (typeof nine);
+ 
+// function 
+// var timeBlock = document.getElementById("#15");
+
+// if (currentTime )
+
 //Able to click into timeblock
 //  -can enter an event into timeblock
 //  -click save button and save event in timeblock
